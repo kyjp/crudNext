0b3bba70717368527466e73e9dd2eba76296e950
kyjp <numunumu17@hotmail.co.jp>
2024-12-13 23:24:08 +0900

    probrem vitest

--- hooks/useSearchHook.spec.tsx
+++ hooks/useSearchHook.spec.tsx
@@ -17,6 +17,8 @@ const mockUseUsersHook = useUsersHook as unknown as Mock
 
 describe('test useSearchHookStore group', () => {
     const setCurrentPageMock = vi.fn()
+    const setSearchQueryMock = vi.fn()
+    const setUsersDataMock = vi.fn()
     beforeAll(() => {
         // useUsersDataStore のモックを設定
         mockUseUsersDataStore.mockImplementationOnce(() => [
@@ -47,9 +49,30 @@ describe('test useSearchHookStore group', () => {
 
     test('should call setCurrentPage when page is changed', () => {
         const { result } = renderHook(() => useSearchHook())
-
         // クリックイベントを模倣
         result.current.handleOnClick({ currentTarget: { innerText: 'Next' }, preventDefault: vi.fn() } as unknown as MouseEvent<HTMLButtonElement>)
         expect(setCurrentPageMock).toHaveBeenCalledWith(1)
     })
+
+    test('should call setSearchQuery when button clicked', () => {
+        const { result } = renderHook(() => useSearchHook())
+
+        // setUsersDataが呼び出されたことを確認
+        expect(setUsersDataMock).toHaveBeenCalled()
+
+        // setUsersDataに渡されたデータの内容を確認
+        expect(mockUseUsersDataStore()).toEqual([
+            {
+                id: '1',
+                name: 'John Doe',
+                created_at: '2019-02-02T00:00:00.000Z',
+                updated_at: '2019-02-02T00:00:00.000Z',
+            }
+        ])
+
+        result.current.handleOnSubmit({ currentTarget: { innerText: '検索' }, preventDefault: vi.fn() } as unknown as MouseEvent<HTMLButtonElement>)
+        expect(setUsersDataMock).toHaveBeenCalledWith(mockUseUsersDataStore())
+        expect(setSearchQueryMock).toHaveBeenCalledWith(1)
+    })
+
 })
